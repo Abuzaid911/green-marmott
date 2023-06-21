4975f48d919928ad079f58605e293c75549cb802
Ahmed Abuzaid <sabobee911@gmail.com>
2023-06-21 22:43:54 +0200

    Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,27 +3,19 @@ function Footer() {
     return (
         <footer className="footer p-10 bg-white text-base-content">
         <div>
-          <span className="footer-title">Services</span> 
-          <a className="link link-hover">Branding</a> 
-          <a className="link link-hover">Design</a> 
-          <a className="link link-hover">Marketing</a> 
-          <a className="link link-hover">Advertisement</a>
-        </div> 
-        <div>
-          <span className="footer-title">Company</span> 
+          <span className="footer-title">Contact</span> 
+          <a className="link link-hover">Emails</a> 
+          <a className="link link-hover">Address</a> 
           <a className="link link-hover">About us</a> 
-          <a className="link link-hover">Contact</a> 
-          <a className="link link-hover">Jobs</a> 
-          <a className="link link-hover">Press kit</a>
         </div> 
         <div>
           <span className="footer-title">Legal</span> 
           <a className="link link-hover">Terms of use</a> 
           <a className="link link-hover">Privacy policy</a> 
-          <a className="link link-hover">Cookie policy</a>
+
         </div> 
         <div>
-          <span className="footer-title">Newsletter</span> 
+          <span className="footer-title">Deals</span> 
           <div className="form-control w-80">
             <label className="label">
               <span className="label-text">Enter your email address</span>
